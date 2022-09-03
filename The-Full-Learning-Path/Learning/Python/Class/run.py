48a144b53f2c792ffb27a7cd7e0fab0afeeb503d
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-03 19:59:10 +0300

    Updated

--- The-Full-Learning-Path/Learning/Python/Class/run.py
+++ The-Full-Learning-Path/Learning/Python/Class/run.py
@@ -1,24 +1,7 @@
-import matplotlib.pyplot as plt 
-import numpy as np
-
-x = range(1,11)
-y = [i**4 for i in x]
-
-
-plt.style.use('Solarize_Light2')
-fig , ax = plt.subplots()
-# ax.plot(x,y, c="red",linewidth=2)
-# ax.scatter(x,y, c="red",s=20)
-# ax.scatter(x, y, c=x, cmap=plt.cm.Blues, s=10)
-ax.bar(x,y, color='green', edgecolor='black', linewidth=1)
-       
-
-ax.set_title("Cube Numbers", fontsize=11)
-ax.set_xlabel("Value", fontsize=11)
-ax.set_ylabel("Cube of Value", fontsize=11)
+word = list('stop')
+anagram = list('pots')
 
 
 
-ax.axis([0 ,15 ,0 ,12000]) # [xmin, xmax, ymin, ymax]
 
-plt.show()
+print(sorted(anagram) == sorted(word))
