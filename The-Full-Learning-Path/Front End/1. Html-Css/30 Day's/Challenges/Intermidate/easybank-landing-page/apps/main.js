437657f355d828b388edf1ffe3227ba52a42833e
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-15 00:57:47 +0300

    update sep 15, 2022

--- The-Full-Learning-Path/Front End/1. Html-Css/30 Day's/Challenges/Intermidate/easybank-landing-page/apps/main.js
+++ The-Full-Learning-Path/Front End/1. Html-Css/30 Day's/Challenges/Intermidate/easybank-landing-page/apps/main.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector("#header");
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (header.classList.contains("open")) {
+    header.classList.remove("open");
+    overlay.classList.add("fade-out");
+    overlay.classList.remove("fade-in");
+  } else {
+    header.classList.add("open");
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+  }
+});
