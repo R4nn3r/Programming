54e32a973f471fea8ec00062acadb3fe2c0a7437
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-21 12:02:30 +0300

    update sep 21, 2022

--- The-Full-Learning-Path/Front End/1. Html-Css/30 Day's/Challenges/Intermidate/easybank-landing-page/apps/main.js
+++ The-Full-Learning-Path/Front End/1. Html-Css/30 Day's/Challenges/Intermidate/easybank-landing-page/apps/main.js
@@ -1,24 +1,24 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const body = document.querySelector("body");
-const header = document.querySelector("#header");
+const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
-const menu = document.querySelector("#menu");
 const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
 
   if (header.classList.contains("open")) {
-    header.classList.remove("open");
+    // Close Hamburger Menu
     body.classList.remove("noscroll");
+    header.classList.remove("open");
     fadeElems.forEach(function (element) {
-      element.classList.add("fade-out");
       element.classList.remove("fade-in");
+      element.classList.add("fade-out");
     });
   } else {
-    header.classList.add("open");
+    // Open Hamburger Menu
     body.classList.add("noscroll");
-
+    header.classList.add("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-out");
       element.classList.add("fade-in");
