c0cb2d9d30edf56bc84156e3b43ec60b96338178
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-28 09:25:13 +0300

    update sep 28, 2022

--- The-Full-Learning-Path/Front End/(on)2. Js/Code/try.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/Code/try.js
@@ -6,14 +6,42 @@ let count = document.getElementById("counter");
 
 function countNum() {
   counter++;
+  console.log("Count: " + counter);
   count.innerHTML = counter;
+  document.querySelector(".num").innerHTML = counter;
+
+  if (counter == 30) {
+    document.querySelector("span").innerHTML = "Done with first month of Js";
+  }
+}
+const heading = document.querySelector("h2");
+
+function hello() {
+  if (heading.innerHTML === "Hello World!") {
+    heading.innerHTML = "GoodBye!";
+    document.querySelector("span").innerHTML = "Day (#1) Done for Today";
+  } else {
+    heading.innerHTML = "Hello World!";
+    document.querySelector("span").innerHTML = "Day (#1)";
+  }
 }
 
-// function displaydate() {
-//   document.getElementById("date").innerHTML = display();
-// }
+const onbtn = document.getElementById("clickBtn");
 
-function changeThebs() {
-  document.querySelector("span").innerHTML = "Day #1 (Done)";
-  document.querySelector("button").innerHTML = "ForToday!";
+function handleClick(e) {
+  console.log("Clicked");
+  onbtn.innerHTML = "Clicked";
+
+  if (onbtn.classList.contains("red")) {
+    onbtn.classList.remove("red");
+    onbtn.innerHTML = "Click";
+  } else {
+    onbtn.classList.add("red");
+  }
 }
+
+onbtn.addEventListener("click", handleClick);
+
+onbtn.addEventListener("click", function (e) {
+  // Functions Code Goes here
+});
