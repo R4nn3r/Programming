1fd8799436f2396d8b4709103e4a3d710ceec7fe
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-29 13:41:46 +0300

    update sep 29

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Projects/Todo/apps/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Projects/Todo/apps/main.js
@@ -1,11 +1,8 @@
-// Get the button and disable it
 const button = document.querySelector("#submit");
 button.disabled = true;
 
-// Get the div with the input area
 const input = document.querySelector("#input");
 
-// Add precaution to the input field
 input.onkeyup = (e) => {
   if (input.value.length > 0) {
     button.disabled = false;
@@ -16,7 +13,6 @@ input.onkeyup = (e) => {
 const form = document.querySelector("form");
 
 form.onsubmit = () => {
-  // Get the input area's value
   const textArea = document.querySelector("#todo");
   console.log(textArea);
 
@@ -32,7 +28,3 @@ form.onsubmit = () => {
 
   return false;
 };
-
-// Create a new div element
-// Assign the value to the div element
-// Clear the test area
