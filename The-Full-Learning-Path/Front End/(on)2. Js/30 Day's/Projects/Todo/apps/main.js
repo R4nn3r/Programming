92d2977f9627a7a2670df8c6cc379b6d151e9183
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-29 16:39:53 +0300

    update sep 29

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Projects/Todo/apps/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Projects/Todo/apps/main.js
@@ -24,7 +24,43 @@ form.onsubmit = () => {
   textArea.appendChild(div);
 
   input.value = "";
+  input.placeholder = "Added! Got More?";
   button.disabled = true;
 
   return false;
 };
+
+const form_2 = document.querySelector("#form_2");
+const button_2 = document.querySelector("#submit_2");
+const todo = form_2.querySelector("#input_2");
+
+button_2.disabled = true;
+const todos = [];
+
+todo.onkeyup = () => {
+  if (todo.value.length > 0) {
+    button_2.disabled = false;
+  } else {
+    button_2.disabled = true;
+  }
+};
+const todo_2 = document.querySelector("#todo_2");
+
+form_2.onsubmit = function (e) {
+  todos.push(todo.value);
+  const div_2 = document.createElement("div");
+
+  todos.forEach(function (todoText) {
+    div_2.innerHTML = todoText;
+    todo_2.appendChild(div_2);
+  });
+
+  todo.value = "";
+  todo.placeholder = "Added! Got More?";
+  button_2.disabled = true;
+
+  return false;
+};
+
+// todos.indexOf("element")
+// delete todos[0]
