59374ac258b9eb6327c8783ae0635efbc9522b87
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-01 22:03:30 +0300

    update oct 1

--- The-Full-Learning-Path/Front End/(on)2. Js/Code/try2.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/Code/try2.js
@@ -1,12 +1,74 @@
 let element = document.createElement('div');
-element.className = "section_div";
+element.classList.add("section_div");
+
+let themeSelector = document.createElement('section');
+themeSelector.id = 'theme';
+themeSelector.className = 'theme';
+
+let themePick1 = document.createElement('input');
+themePick1.type = 'checkbox';
+themePick1.id = 'themePick1';
+themePick1.className = 'themePick';
+let themePick2 = document.createElement('input');
+themePick2.type = 'checkbox';
+themePick2.id = 'themePick2';
+themePick2.className = 'themePick';
+
+
+let themePick3 = document.createElement('input');
+themePick3.type = 'checkbox';
+themePick3.id = 'themePick3';
+themePick3.className = 'themePick';
+
+
+themeSelector.appendChild(themePick1);
+themeSelector.appendChild(themePick2);
+themeSelector.appendChild(themePick3);
 
 
 let h1 = document.createElement('h1');
 h1.innerHTML = 'Hello World!';
+h1.style.width = '100%';
 
-element.appendChild(h1);
+let button = document.createElement('button');
+button.innerHTML = 'Hello World!';
+button.id = 'button';
+button.className = "button";
+
+element.appendChild(button);
+element.prepend(h1);
+element.appendChild(themeSelector);
+
 
 const main = document.querySelector('.main');
 
 main.appendChild(element);
+
+
+button.addEventListener("click", () => {
+  const background = document.querySelector('body');
+  
+  background.classList.toggle('red');
+  
+});
+
+themePick1.addEventListener("change", () => {
+  const background = document.querySelector('body');
+  
+  background.classList.toggle('green');
+  
+});
+
+themePick2.addEventListener("change", () => {
+  const background = document.querySelector('body');
+  
+  background.classList.toggle('yellow');
+  
+});
+
+themePick3.addEventListener("change", () => {
+  const background = document.querySelector('body');
+  
+  background.classList.toggle('lime');
+  
+});
