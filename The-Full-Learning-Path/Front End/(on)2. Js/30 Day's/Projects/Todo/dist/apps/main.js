a7fbcdfc4225cd94bfe6710f2ad60cbe14943556
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-01 14:49:34 +0300

    update oct 1, 2022

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Projects/Todo/dist/apps/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Projects/Todo/dist/apps/main.js
@@ -45,19 +45,31 @@ const createTodo = (title, dueDate) => {
 };
 
 // Deletes a todo
-const removeTodo = (idToDelete) => {
-  todos = todos.filter((todo) => {
-    // If the id of this todo matches idToDelete, return false
-    // For everything else, return true
+// Deletes a todo
+function removeTodo(idToDelete) {
+  todos = todos.filter(function (todo) {
     if (todo.id === idToDelete) {
       return false;
     } else {
       return true;
     }
   });
-
   saveTodos();
-};
+}
+
+function markChecked(todoId, checkId) {
+  todos.forEach(function (todo) {
+    const lineThrough = document.getElementById(todo.id);
+    if (lineThrough.classList.contains("list")) {
+      console.log(lineThrough);
+      lineThrough.classList.replace("list", "line");
+    }
+    if (todo.id === todoId) {
+      todo.isDone = checkId;
+    }
+  });
+  saveTodos();
+}
 
 const saveTodos = () => {
   localStorage.setItem("todos", JSON.stringify(todos));
@@ -85,6 +97,16 @@ const deleteTodo = (event) => {
   render();
 };
 
+const addChecked = (event) => {
+  const checkBox = event.target;
+
+  const todoId = checkBox.dataset.todoId;
+  const checkId = checkBox.checked;
+
+  markChecked(todoId, checkId);
+  render();
+};
+
 // View
 const render = () => {
   // reset our list
@@ -92,7 +114,22 @@ const render = () => {
 
   todos.forEach((todo) => {
     const element = document.createElement("div");
-    element.innerText = todo.title + " " + todo.dueDate;
+    element.id = todo.id;
+    element.className = "list";
+    element.innerText = todo.title + " - " + todo.dueDate;
+
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.className = "checkbox";
+    checkBox.onchange = addChecked;
+    checkBox.dataset.todoId = todo.id;
+    if (todo.isDone === true) {
+      checkBox.checked = true;
+    } else {
+      checkBox.checked = false;
+    }
+
+    element.prepend(checkBox);
 
     const deleteButton = document.createElement("div");
     deleteButton.onclick = deleteTodo;
@@ -102,8 +139,17 @@ const render = () => {
 
     const todoList = document.getElementById("todo-list");
     todoList.appendChild(element);
+
     button_todo.disabled = true;
   });
 };
 
 render();
+
+const button = document.querySelector("[mobile-toggle]");
+
+button.addEventListener("click", () => {
+  const main = document.querySelector(".main");
+
+  main.classList.toggle("main__hidden");
+});
