d398a033bb4a38fd57bff09a441e0729e73a0648
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-03 22:13:29 +0300

    update oct 3

--- The-Full-Learning-Path/Front End/(on)2. Js/Code/try2.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/Code/try2.js
@@ -313,18 +313,22 @@ select.addEventListener("change", function (e) {
 
   switch (choice) {
     case "sunny":
-      para.textContent = "Sunny";
+      para.textContent =
+        "It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.";
       break;
     case "rainy":
-      para.textContent = "Rainy";
+      para.textContent =
+        "Rain is falling outside; take a rain coat and an umbrella, and don't stay out for too long.";
       break;
     case "snowing":
-      para.textContent = "Snowing";
+      para.textContent =
+        "The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.";
       break;
     case "overcast":
-      para.textContent = "Overcast";
+      para.textContent =
+        "It isn't raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.";
       break;
     default:
-      para.textContent = "Unknown";
+      para.textContent = " ";
   }
 });
