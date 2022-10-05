e6d8bba2835cda1f5da73ce1abe556f0435385d7
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-05 17:46:38 +0300

    update oct 5

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Newbie/(on)faq-accordion-card-main/app/js/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Newbie/(on)faq-accordion-card-main/app/js/main.js
@@ -3,6 +3,9 @@ const quest = document.querySelectorAll(".boxxx");
 quest.forEach((q) => {
   console.log(q);
   q.addEventListener("click", () => {
+    const title = q.querySelector("h4");
+    title.classList.toggle("selected");
+
     const content = q.querySelector("#content");
     console.log(content);
     content.classList.toggle("hidden");
