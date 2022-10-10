03178382bb5a9072ce849fa241af71298c5e4b10
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-10 15:52:11 +0300

    update oct 10, 2022

--- The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa_3.cpp
+++ The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa_3.cpp
@@ -3,14 +3,18 @@
 using namespace std;
 
 int main(){
+    int k = 0;
+    int n;
 
-    for (int i = 0; i < 5; i++) {
-        for (int j = -1; j < i; j++){
-            cout << "#";
-        }
-        cout<< endl;
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        k++;
     }
 
+    cout << k << endl;
+    
     return 0;
 }
 
