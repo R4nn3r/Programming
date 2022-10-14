cd4f1a162fcd9b73e3b3dcaf7c7a63a29852ca61
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-14 10:31:29 +0300

    update

--- The-Full-Learning-Path/Front End/(on)2. Js/Code/two/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/Code/two/main.js
@@ -40,6 +40,80 @@ const cards1 = [2, 3, 4, 5, 6];
 const cards2 = [7, 8, 9];
 const cards3 = [10, "J", "Q", "K", "A"];
 
-console.log(cc(cards1));
-console.log(cc(cards2));
-console.log(cc(cards3));
+// console.log(cc(cards1));
+// console.log(cc(cards2));
+// console.log(cc(cards3));
+
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+];
+
+// console.log(myMusic);
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop != "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else {
+    if (records[id].hasOwnProperty(prop) && value !== "") {
+      records[id][prop].push(value);
+    } else if (!records[id].hasOwnProperty(prop) && value !== "") {
+      records[id][prop] = [];
+      records[id][prop].push(value);
+    } else {
+      delete records[id][prop];
+    }
+  }
+  return records;
+}
+
+// updateRecords(recordCollection, 5439, "artist", "ABBA");
+updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me");
+// updateRecords(recordCollection, 5439, "tracks", "");
+
+// console.log(recordCollection);
