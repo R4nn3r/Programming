e3adaff84b48d3b69cff0204b4db2d872f2422cb
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-16 11:57:41 +0300

    update

--- The-Full-Learning-Path/Front End/(on)2. Js/Code/three/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/Code/three/main.js
@@ -5,3 +5,34 @@ const inputs = themes.querySelectorAll(".check");
 themes.addEventListener("change", (e) => {
   body.classList.toggle(e.target.value);
 });
+
+const btn = document.querySelector("#btnAdd");
+
+const input = document.querySelector("#text");
+
+btn.addEventListener("click", () => {
+  const li = document.createElement("li");
+
+  const p = document.createElement("p");
+  p.className = "para";
+  p.innerText = input.value;
+  li.appendChild(p);
+
+  const divRemove = document.createElement("div");
+  divRemove.className = "done";
+  divRemove.innerHTML = "x";
+  li.appendChild(divRemove);
+
+  const divDone = document.createElement("div");
+  divDone.className = "remove";
+  divDone.innerHTML = "x";
+  li.appendChild(divDone);
+
+  const div = document.getElementById("items");
+
+  const ul = div.firstElementChild;
+
+  ul.appendChild(li);
+
+  input.value = "";
+});
