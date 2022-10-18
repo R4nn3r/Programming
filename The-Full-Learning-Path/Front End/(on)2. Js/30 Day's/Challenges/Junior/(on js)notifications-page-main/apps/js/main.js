41c24f3ddf6bff2de3b297c107904996a40a29df
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-18 12:18:59 +0300

    update

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on js)notifications-page-main/apps/js/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on js)notifications-page-main/apps/js/main.js
@@ -5,7 +5,17 @@ const body = document.querySelector("body");
 const theme = ["light", "brown", "dark"];
 
 checkBox.addEventListener("change", (e) => {
-  let val = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-  body.className = theme[val];
-  console.log(val);
+  body.className = theme[getRand()];
 });
+
+var getRand = (function () {
+  var nums = [0, 1, 2];
+  var current = [];
+  function rand(n) {
+    return (Math.random() * n) | 0;
+  }
+  return function () {
+    if (!current.length) current = nums.slice();
+    return current.splice(rand(current.length), 1);
+  };
+})();
