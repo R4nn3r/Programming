303b9e25e1b4779bd1af037389ee8896eeceb563
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-10-24 15:24:27 +0300

    update

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on)intro-section-with-dropdown-navigation-main/apps/js/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on)intro-section-with-dropdown-navigation-main/apps/js/main.js
@@ -1,3 +1,10 @@
+// toggle the hamburger icons to closed and opend
 function myFunction(x) {
   x.classList.toggle("change");
+}
+
+// Open The drop down list and change the arrow icon
+function openTree(elem) {
+  elem.firstElementChild.classList.toggle("up");
+  elem.nextElementSibling.classList.toggle("treeDown");
 }
