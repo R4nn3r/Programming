7597f6cf34fe0daa3d1249e0ab53fbc9cba87dda
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-11-07 10:38:07 +0300

    update

---
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(done)blogr-landing-page-main/dist-2nd/apps/js/main.js
@@ -0,0 +1,35 @@
+/* const dropDown = document.querySelector(".drop-down");
+dropDown.className = "drop-down";
+const arrow = document.querySelector(".arrow");
+arrow.className = "arrow";
+
+function toggle(item) {
+  const arrow = item.firstElementChild;
+  const drop = item.parentNode.lastElementChild;
+
+  arrow.classList.toggle("opened");
+  drop.classList.toggle("closed");
+}
+ */
+
+const toggler = document.querySelectorAll(".toggler");
+
+toggler.forEach((toggle) => {
+  toggle.addEventListener("click", (e) => {
+    e.target.firstElementChild.classList.toggle("opened");
+    e.target.parentNode.lastElementChild.classList.toggle("drop");
+  });
+
+  // blur event (only one dropdown can be active at a time)
+  toggle.addEventListener("blur", (event) => {
+    const arrowTwo = event.target.firstElementChild.classList.remove("opened");
+    const container =
+      event.target.parentNode.lastElementChild.classList.remove("drop");
+  });
+});
+
+// Hamburger Menu
+function hamToggle(menu) {
+  menu.classList.toggle("close");
+  menu.parentNode.querySelector(".inactive").classList.toggle("negate");
+}
