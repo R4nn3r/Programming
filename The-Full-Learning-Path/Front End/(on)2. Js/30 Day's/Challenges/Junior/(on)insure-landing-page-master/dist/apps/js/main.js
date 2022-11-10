44a0bae72907bef56795d4e54065cd7aca297554
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-11-10 21:43:09 +0300

    update

---
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on)insure-landing-page-master/dist/apps/js/main.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("body");
+
+// Hamburger Menu Switch
+const HamClick = (ham) => {
+  ham.firstElementChild.classList.toggle("active");
+  ham.lastElementChild.classList.toggle("active");
+  ham.nextElementSibling.classList.toggle("active");
+  body.classList.toggle("overflow-paused");
+};
+
+function theme() {
+  body.classList.toggle("light-theme");
+}
