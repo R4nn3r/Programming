96ffea789ee8bcdd73b986549468b9489c1ef8fb
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-11-13 12:42:30 +0300

    update

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on)interactive-pricing-component-main/apps/js/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on)interactive-pricing-component-main/apps/js/main.js
@@ -1,6 +1,7 @@
 const input = document.querySelector("#checkbox");
 const price = document.querySelector(".price");
 const view = document.querySelector(".view");
+const minSlide = document.querySelector(".min-slide");
 
 // input.addEventListener("checked", () => {
 //   let value = parseInt(price.innerHTML);
@@ -11,7 +12,9 @@ function check(input) {
   if (input.checked) {
     let newValue = parseInt(price.innerHTML);
     price.innerHTML = `${newValue - newValue * 0.25}.00`;
+    minSlide.style.backgroundColor = " var(--slide-bg)";
   } else {
     price.innerHTML = value;
+    minSlide.style.backgroundColor = " var(--toggle-bg)";
   }
 }
