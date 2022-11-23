066253bad001ad1974d66ff5d9334198e8282860
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-11-23 20:01:58 +0300

    update

--- The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa.cpp
+++ The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa.cpp
@@ -39,18 +39,40 @@ Add a new node to the linked list from the Front
     3, make the next of the new node the head 
     4, move the head pointer to the new node
  */
-void add_new_node(Node* head){
+void add_to_front(Node* temp){
     Node* new_node = new Node();
     new_node->data = "new_node";
 
-    if(head != NULL){
-        new_node->next = head;
-        head = new_node;
+    if(temp != NULL){
+        new_node->next = temp;           
+        temp = new_node;
     }else{
+        cout << "check failed" << endl;
+        temp->next = new_node;
         new_node->next = NULL;
-        head = new_node;
     }
 }
+
+/* 
+    Add to the end of the list
+ */
+void add_to_back(Node* temp){
+    Node* new_back_node = new Node();
+    new_back_node->data = "end of the list";
+
+     if (temp != NULL){
+        while(temp != NULL){
+            if (temp->next == NULL){
+                temp->next = new_back_node;
+                new_back_node->next = NULL;
+            }
+            temp = temp->next;
+        }
+    }else{
+        cout << "List is empty" << endl;
+    }
+}
+
 
 int main(){
 
@@ -64,38 +86,30 @@ int main(){
     head->data = "head";
     head->next = first;
 
-    // 1st
+    // // 1st
     first->data = "first";
     first->next = second;
 
-    // 2nd
+    // // 2nd
     second->data = "second";
     second->next = third;
 
-    // 3rd
+    // // 3rd
     third->data = "third";
     third->next = NULL;
 
     
     
     // Add a new node to the front of the list 
-    // add_new_node(head);
-
-    // Traverse the Linked List 
-    traverse_list(head);
+    add_to_front(head);
 
     // Add a new node to the end of the list 
-    Node* new_back_node = new Node();
-    new_back_node->data = "end of the list";
+    // add_to_back(head);
 
-    Node* temp = new Node();
-    temp = head;
 
-    while(temp->next = NULL){
-        temp->next = new_back_node;
-        new_back_node->next = NULL;
-    }
- 
+    // Traverse the Linked List 
+    cout << "\nafter the adding the node to the end \n" << endl;
+    traverse_list(head);
 
     return 0;
 }
