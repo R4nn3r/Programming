269f782d5338d9733f8b07101311403425d9b449
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-11-23 19:25:40 +0300

    update

--- The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa.cpp
+++ The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa.cpp
@@ -1,78 +1,92 @@
-// Linked List's 
+// Singly Linked List implementation 
 #include <iostream>
 
 using namespace std;
 
-class Node {
-
-    public: 
-        int value;
-        Node* next;
-
+// Create the Structure of the linked list 
+struct Node {
+    string data;
+    Node* next;
 };
 
-// Function to read the value of Linked Lists Node's 
-void printNodes(Node* temp){
-    while(temp != NULL){
-        cout << temp->value << endl;
-        temp = temp->next;
-
+/* 
+Traverse the linked list cout the list node data's
+    1, Check if the List is empty or not
+        - create a temp node to keep track of the head
+        - assign the temp to the head 
+        - create a while loop that traverses all the way through the list until the next pointer points to a NULL value cause only the last node has next set to NULL
+            - cout the temp->data 
+            - set the temp value to the next node
+    2. if list is empty
+        - cout "the list is empty"
+ */
+void traverse_list(Node* temp){
+    if (temp != NULL){
+        while(temp != NULL){
+            cout << temp->data << endl;
+            temp = temp->next;
+        }
+    }else{
+        cout << "List is empty" << endl;
     }
 }
 
 
+/* 
+Add a new node to the linked list from the Front
+    1, Allocate a new node
+    2, assign the date to the new node 
+    3, make the next of the new node the head 
+    4, move the head pointer to the new node
+ */
+void add_new_node(Node* head){
+    Node* new_node = new Node();
+    new_node->data = "new_node";
+
+    if(head != NULL){
+        new_node->next = head;
+        head = new_node;
+    }else{
+        new_node->next = NULL;
+        head = new_node;
+    }
+}
 
 int main(){
 
-    // Creating a new Node pointers 
+    // Create the nodes and assign data and next
     Node* head = new Node();
+    Node* first = new Node();
     Node* second = new Node();
     Node* third = new Node();
 
+    // Head
+    head->data = "head";
+    head->next = first;
 
-    // Adding value to the Node's
+    // 1st
+    first->data = "first";
+    first->next = second;
 
-    // First Node 
-    // [->] is used to access values while using pointers 
-    head->value = 10;
-    head->next = second;
-
-    // Second Node
-    second->value = 20;
+    // 2nd
+    second->data = "second";
     second->next = third;
 
-    // Third Node
-    third->value = 30;
+    // 3rd
+    third->data = "third";
     third->next = NULL;
 
-
-    Node* newNode = new Node();
-    newNode->value = 696969;
-
-
-    newNode->next = head;
-    head = newNode;
+    
+    
+    // Add a new node to the front of the list 
+    add_new_node(head);
+ 
 
 
-
-
-    //  Print the node from with is the main function by creating a temp node that tracks the head
-    Node* temp = new Node();
-    temp = head;
-
-    while(temp != NULL){
-        cout << temp->value << endl;
-        temp = temp->next;
-    }
-
-    cout << "n/n/";
-
-    cout << "Output from the outside Function: " << endl;
-    printNodes(head);
+    // Traverse the Linked List 
+    traverse_list(head);
 
 
 
     return 0;
 }
-
-
