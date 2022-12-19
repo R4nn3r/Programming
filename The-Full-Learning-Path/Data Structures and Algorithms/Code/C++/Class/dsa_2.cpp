9a5fee0281bc3ae2a9f822b5c21d2a528ec7bd6b
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-12-19 22:06:43 +0300

    update

--- The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa_2.cpp
+++ The-Full-Learning-Path/Data Structures and Algorithms/Code/C++/Class/dsa_2.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+int sort(int arr[], int size){
+
+
+  return 1
+}
+
 
 int main(){
+  int arr[] = {11,9,21,8,17,19,1,24,12};
+
+  int size = sizeof(arr)/sizeof(arr[0]) - 1;
+
+
+  if(sort(arr, size)){
+    cout << "Sorted -: ";
+    for(int i = 0; i < size; i++){
+      cout << arr[i] << " ";
+    }
+    cout << endl;
+  }
 
 
   return 0;
