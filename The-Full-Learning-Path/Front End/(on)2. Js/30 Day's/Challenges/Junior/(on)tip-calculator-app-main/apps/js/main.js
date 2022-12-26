fe7f312c6bf18923cb484b2e1adb47106fa2b0b9
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-12-26 16:58:58 +0300

    update

--- The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on)tip-calculator-app-main/apps/js/main.js
+++ The-Full-Learning-Path/Front End/(on)2. Js/30 Day's/Challenges/Junior/(on)tip-calculator-app-main/apps/js/main.js
@@ -1,3 +1,7 @@
+/* Class */
+class Calculator {}
+
+/* Selectors */
 const billInput = document.querySelector("[data-bill-input]");
 const peopleInput = document.querySelector("[data-people-input]");
 
@@ -8,3 +12,6 @@ const tipAmount = document.querySelector("[data-tip]");
 const totalAmount = document.querySelector("[data-total]");
 
 const resetButton = document.querySelector("[data-reset]");
+
+/* Creating an object from the class */
+const calculator = new Calculator();
