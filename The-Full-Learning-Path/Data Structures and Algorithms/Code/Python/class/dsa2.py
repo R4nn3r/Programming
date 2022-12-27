a48a25c4471739569134f71578e39b5f78c17635
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-12-27 07:32:52 +0300

    update

--- The-Full-Learning-Path/Data Structures and Algorithms/Code/Python/class/dsa2.py
+++ The-Full-Learning-Path/Data Structures and Algorithms/Code/Python/class/dsa2.py
@@ -1,18 +1,5 @@
-def linear_search(list, target):
-    """ returns the index position of the target if found , else returns none """
-
-    for i in range(len(list)):
-        if list[i] == target:
-            return i
-    return None
-
-def verify(index):
-    if index is not None:
-        print(f"found at {index}")
-    else:
-        print("target not found")
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-result = linear_search(numbers, 6)
-
-verify(result)
+def main():
+    ...
+    
+if __name__ == '__main__':
+    main()
--- The-Full-Learning-Path/Data Structures and Algorithms/Code/Python/class/dsa3.py
+++ The-Full-Learning-Path/Data Structures and Algorithms/Code/Python/class/dsa3.py
@@ -1,27 +1,5 @@
-def binary_search(list, target):
-    """ binary search """
-    first = 0
-    last = len(list) - 1
-
-
-    while first <= last:
-        midpoint = (first + last) // 2
-        if list[midpoint] == target:
-            return midpoint
-        elif list[midpoint] < target:
-            first = midpoint + 1
-        else:
-            last = midpoint - 1
-    return None
-
-
-def verify(index):
-    if index is not None:
-        print(f"found at {index}")
-    else:
-        print("target not found")
-
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13]
-result = binary_search(numbers, 10)
-
-verify(result)
+def main():
+    ...
+    
+if __name__ == '__main__':
+    main()
