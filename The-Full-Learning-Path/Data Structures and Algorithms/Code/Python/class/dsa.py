3dbaf66df3a2975b6eb23da9d6299671ceac0f1c
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-12-27 09:42:55 +0300

    update

--- The-Full-Learning-Path/Data Structures and Algorithms/Code/Python/class/dsa.py
+++ The-Full-Learning-Path/Data Structures and Algorithms/Code/Python/class/dsa.py
@@ -1,5 +1,21 @@
+def isAnagram(s, t):
+    if len(s) != len(t): return False
+
+    bank = []
+    for word in s: 
+        if word in t:
+            bank.append(word)
+
+
+    if len(bank) == len(s) or len(bank) == len(t): return True
+    return False
+
+
+
 def main():
-    ...
-    
+    s = "light"
+    t = "tight"
+    print(isAnagram(s, t))
+
 if __name__ == '__main__':
     main()
