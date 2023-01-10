7ae565db4ca8331c7609cf481e622a235899a37d
Yonatan Afewerk <yonatanafewerk@gmail.com>
2023-01-11 00:06:07 +0300

    update

--- The-Full-Learning-Path/Learning/Java/class/Try3.java
+++ The-Full-Learning-Path/Learning/Java/class/Try3.java
@@ -1,15 +1,15 @@
 public class Try3 {
   public static void main(String[] args) {
-    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7, 8} };
 
-    for(int i = 0; i < myNumbers.length; i++) {
-      for (int j = 0; j < myNumbers[i].length; j++){
-        System.out.print(myNumbers[i][j] + " ");
-      }
-      System.out.println();
-    }
+    // Conditional 
+    int myAge = 25;
+    int votingAge = 18;
 
-    System.out.println();
+
+    String var = (myAge >= votingAge) ? "Old enough to vote!": "Not old enough to vote.";
+    System.out.println(var);
+
+    // Array 
 
     String [] cars = {"BMW","Mazda", "Volvo"};
     int carsLength = cars.length;
@@ -19,6 +19,19 @@ public class Try3 {
     }
 
     System.out.println(carsLength);
+
+
+    // Multi Dimensional Array
+
+    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7, 8} };
+
+    for(int i = 0; i < myNumbers.length; i++) {
+      for (int j = 0; j < myNumbers[i].length; j++){
+        System.out.print(myNumbers[i][j] + " ");
+      }
+      System.out.println();
+    }
+
 
   }
 }
